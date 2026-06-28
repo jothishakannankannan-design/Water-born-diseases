4b86c8487bb9cf4990c0bfd06d61d1c05e963430
Jothisha Kannan Sudha <jothishakannankannan@gmail.com>
2026-06-28 21:56:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function checkRisk() {
+
+    let village = document.getElementById("village").value;
+    let water = Number(document.getElementById("water").value);
+    let ph = Number(document.getElementById("ph").value);
+    let turbidity = Number(document.getElementById("turbidity").value);
+
+    let risk = "";
+    let advice = "";
+
+    if (water >= 80 && ph >= 6.5 && ph <= 8.5 && turbidity <= 5) {
+        risk = "LOW";
+        advice = "✅ Water is safe for drinking.";
+    } else if (water >= 50) {
+        risk = "MEDIUM";
+        advice = "⚠️ Boil water before drinking.";
+    } else {
+        risk = "HIGH";
+        advice = "❌ Unsafe water. Immediate treatment is required.";
+    }
+
+    document.getElementById("villageResult").innerHTML = village;
+    document.getElementById("waterResult").innerHTML = water + "%";
+    document.getElementById("risk").innerHTML = risk;
+    document.getElementById("advice").innerHTML = advice;
+      }
